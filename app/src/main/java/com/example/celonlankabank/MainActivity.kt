fd445c99c1ad265b9996bd36a810fca65fb15c0c
nethmichamathka <chamathkanethmi11@gmail.com>
2024-11-27 11:59:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/celonlankabank/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.celonlankabank
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main) // Make sure this matches your XML layout file name
+    }
+}
